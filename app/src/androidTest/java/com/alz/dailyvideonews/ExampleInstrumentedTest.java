1d398990e118be1b34320376fce5ef917156b1c3
Alz-Android <apiran06@gmail.com>
2016-11-06 15:27:01 -0800

    initial commit webViw works bug: back button needs 2 presses

--- app/src/androidTest/java/com/alz/dailyvideonews/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/alz/dailyvideonews/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.alz.dailyvideonews", appContext.getPackageName());
+        assertEquals("com.alz.testtesting", appContext.getPackageName());
     }
 }
