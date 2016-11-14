e7eb6dda57566267127e606f24b98d97e42a342e
Alz-Android <apiran06@gmail.com>
2016-11-14 12:31:44 -0800

    feature: jks file added

--- app/src/main/java/com/alz/dailyvideonews/MainActivity.java
+++ app/src/main/java/com/alz/dailyvideonews/MainActivity.java
@@ -107,7 +107,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     startActivity(intent, bundle);
                 }
                 else {
-
                     startActivity(intent);
                 }
             }
